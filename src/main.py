8f7ceccb9b3bbb92d58d1d971884178518cdee85
MB <michal@boruch.xyz>
2020-11-25 19:16:06 +0100

    Setup docker networking.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello you!")
+import http.server
+import socketserver
+
+PORT = 8001
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+httpd.serve_forever()
